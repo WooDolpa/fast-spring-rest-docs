a5d6aa11255dbb9874c71014f7cacf6a8b6c77b7
jwlee <dannis1124@naver.com>
2023-02-14 09:48:56 +0900

    spring rest docs project init...

---
+++ src/main/java/com/rest/docs/Application.java
@@ -0,0 +1,17 @@
+package com.rest.docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file           : Application
+ * author         : jwlee
+ * date           : 2023-02-14
+ * description    :
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
