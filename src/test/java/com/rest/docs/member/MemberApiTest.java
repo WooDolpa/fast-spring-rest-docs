0fa8890afd17c3b435e16e5e2a6ace565498c1b2
jwlee <dannis1124@naver.com>
2023-02-22 23:53:53 +0900

    test case

---
+++ src/test/java/com/rest/docs/member/MemberApiTest.java
@@ -0,0 +1,14 @@
+package com.rest.docs.member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName : com.rest.docs.member
+ * className : MemberApiTest
+ * user : jwlee
+ * date : 2023/02/22
+ * description :
+ */
+class MemberApiTest {
+
+}
